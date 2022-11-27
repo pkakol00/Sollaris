f83b912d492302e282d1577003fcb9ad8fc30183
LokiNaBoki <jedrzejkowski@protonmail.com>
2022-11-27 18:05:30 +0100

    SOL-2: add GE constructor

--- sollaris/src/physics/PhysicsEngine.cpp
+++ sollaris/src/physics/PhysicsEngine.cpp
@@ -1,15 +1,4 @@
-#ifndef __PHYSICSENINE_H__
-#define __PHYSICSENINE_H__
+#include "PhysicsEngine.h"
 
-#include "../util/events.h"
-#include <vector>
-
-class PhysicsEngine{
-public:
-  void update();
-  CREATE_EVENT(planet_merge, void, std::vector<int>, int);
-  CREATE_EVENT(planet_division, int, std::vector<int>);
-
-};
-
-#endif // __PHYSICSENINE_H__
+PhysicsEngine::PhysicsEngine(std::shared_ptr<std::vector<PlanetPosition>> position, std::shared_ptr<std::vector<PlanetData>> data, double interval)
+  : position(position), data(data), interval(interval), G(6.6743015e-10) {}
--- sollaris/src/physics/PhysicsEngine.h
+++ sollaris/src/physics/PhysicsEngine.h
@@ -2,10 +2,17 @@
 #define __PHYSICSENINE_H__
 
 #include "../util/events.h"
+#include "../util/common.h"
 #include <vector>
+#include <memory>
 
-class PhysicsEngine{
+class PhysicsEngine {
+  std::shared_ptr<std::vector<PlanetPosition>> position;
+  std::shared_ptr<std::vector<PlanetData>> data;
+  double interval;
+  const double G;
 public:
+  PhysicsEngine(std::shared_ptr<std::vector<PlanetPosition>>, std::shared_ptr<std::vector<PlanetData>>, double);
   void update();
   CREATE_EVENT(planet_merge, void, std::vector<int>, int);
   CREATE_EVENT(planet_division, int, std::vector<int>);
